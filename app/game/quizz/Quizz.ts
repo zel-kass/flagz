a1677ba8cf7c0c05ddbaed64ded8623ca8ea9880
JeanJano <jeansauvage6@gmail.com>
2025-02-18 15:56:40 +0100

    add newLevel function

--- app/game/quizz/Quizz.ts
+++ app/game/quizz/Quizz.ts
@@ -14,12 +14,9 @@ export default class Quizz {
     private _typedKeyMap: KeyMap = keyMap;
     private _typedFlagMap: FlagMap = flagMap;
     private _score: number;
-    // private seen_flags: string[];
     private _actual_flag: string;
     private _solutions: Map<string, string>;
-    // private good_answer: string;
-    // private bad_answer: string[];
-    
+    // private seen_flags: string[];
 
     constructor() {
         this._score = 0;
@@ -29,17 +26,32 @@ export default class Quizz {
         this._setSolutions();
     }
 
+    public upScore() { this._score += 1; }
+
+    public newLevel() {
+        console.log(this._solutions)
+        this._solutions.clear()
+        console.log(this._solutions)
+        const key: number = this._createRandomKey();
+        this._actual_flag = this._typedKeyMap[key.toString()];
+        this._setSolutions();
+    }
+
     private _setSolutions() {
-        const random: number = Math.floor(Math.random() * 4);
+        const random: number = Math.floor(Math.random() * 3);
         for (let i: number = 0; i < 4; i++) {
             if (i == random)
                 this._solutions.set(this._actual_flag, this._typedFlagMap[this._actual_flag]);
             else {
-                const key: string = this._typedKeyMap[this._createRandomKey().toString()];
+                let key: string = "xy";
+                while (key == "xy" || this._solutions.has(key))
+                    key = this._typedKeyMap[this._createRandomKey().toString()];
                 this._solutions.set(key, this._typedFlagMap[key]);
             }
         }
     }
+
+
 
     private _createRandomKey() : number {
         return Math.floor(Math.random() * 197);
--- app/game/quizz/page.tsx
+++ app/game/quizz/page.tsx
@@ -1,17 +1,34 @@
 'use client';
+
+import { useState, useEffect } from 'react'
 import Quizz from "./Quizz";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
 
 export default function QuizzView() {
+  const [score, setScore] = useState(0);
+  const [quizz, setQuizz] = useState<Quizz | null>(null);
 
-  const quizz: Quizz = new Quizz();
-  console.log(quizz);
+  useEffect(() => {
+    setQuizz(new Quizz());
+  }, []);
+
+  function handleSubmit(key: string) {
+    if (quizz && key === quizz.getActualFlag()) {
+      quizz.upScore();
+      setScore(quizz.getScore());
+      quizz.newLevel();
+    } else {
+      alert("T'as perdu gros cake!");
+    }
+  }
+
+  if (!quizz) return;
 
   return (
     <div className="flex items-center justify-center flex-col gap-y-2">
-      <h2>Score: {quizz.getScore()}</h2>
+      <h2>Score: {score}</h2>
       <Image
         src={`/assets/${quizz.getActualFlag()}.svg`}
         alt="current flag"
@@ -20,7 +37,7 @@ export default function QuizzView() {
       />
       <div className="grid grid-cols-2 grid-rows-2 gap-2">
         {Array.from(quizz.getSolutions()).map(([key, value]) => (
-          <Button key={key} className="w-full">
+          <Button onClick={() => handleSubmit(key)} key={key} className="w-full">
               {value}
             </Button>
           ))}
